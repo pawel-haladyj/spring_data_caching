333499c381b4b7ed6118d1fb163a9f98541b7e25
Pawel Haladyj <pawel@haladyj.pl>
2020-03-22 18:28:20 +0100

    initial commit

---
+++ src/main/java/pl/haladyj/spring_data_caching/SpringDataCachingApplication.java
@@ -0,0 +1,13 @@
+package pl.haladyj.spring_data_caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataCachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/haladyj/spring_data_caching/SpringDataCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.spring_data_caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
